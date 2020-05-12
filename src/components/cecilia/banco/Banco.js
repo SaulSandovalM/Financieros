3134f3acf80091f8502b2a56810e6e451ba296be
Feracrz <hitclear23@hotmail.com>
2020-05-11 22:40:33 -0500

    aqui peleando

---
+++ src/components/cecilia/banco/Banco.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import firebase from '../../../Firebase';
+import './Banco.css';
+
+export default class Banco extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lista: [
+        {
+          id: 1,
+          name: 'preuba',
+          done: false
+        },
+      ],
+      form: [],
+      numero: '',
+      monto: '',
+      fechaC: '',
+      fechaE: '',
+      estatus: '',
+      usuario: '',
+      banco: '',
+      contador: {},
+    };
+  }
+
+  listenForItems = (itemsRef) => {
+    itemsRef.on('value', (snap) => {
+      var lista = [];
+      snap.forEach((child) => {
+        lista.push({
+          numero: child.val().numero,
+          monto: child.val().monto,
+          fechaC: child.val().fechaC,
+          fechaE: child.val().fechaE,
+          estatus: child.val().estatus,
+          usuario: child.val().usuario,
+          banco: child.val().banco,
+          done: child.val().done,
+          id: child.key
+        });
+      });
+      this.setState({
+        lista: lista
+      });
+    });
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('Caja/');
+    this.listenForItems(itemsRef);
+    this.consumo();
+    setInterval(this.consumo, 500);
+  }
+
+  componentWillMount() {
+    let formRef = firebase.database().ref('Caja').orderByKey().limitToLast(6);
+    formRef.on('child_added', snapshot => {
+      const { numero, monto, fechaC, fechaE, estatus, usuario, banco } = snapshot.val();
+      const data = { numero, monto, fechaC, fechaE, estatus, usuario, banco };
+      this.setState({ form: [data].concat(this.state.form) });
+    });
+  }
+
+  consumo = () => {
+    const ref = firebase.firestore().collection('Caja').doc('--stats--');
+    ref.get().then((doc) => {
+      if (doc.exists) {
+        this.setState({
+          contador: doc.data(),
+          key: doc.id,
+          isLoading: false
+        });
+      } else {
+        console.log("No such document!");
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div class='container-back'>
+        <div class='site'>
+          <p class='site-s'><b>Banco</b></p>
+        </div>
+        <div class='caja-container'>
+          <div class='caja-inputs'>
+            <div class='caja-inputs-c'>
+              <div class='input-row'>
+              </div>
+              <div class='input-row'>
+              </div>
+              <div class='input-row'>
+              </div>
+              <div class='input-row'>
+              </div>
+            </div>
+            <div class='disponible'>
+              <div>
+                <p class='p-caja-dis'><b>SALDO DISPONIBLE</b></p>
+                <p class='cantidad-caja'>MXN $1,000,000.00</p>
+              </div>
+            </div>
+          </div>
+
+          <div class='caja-inputs' style={{marginBottom: '40px'}}>
+            <div class='caja-inputs-c'>
+              <div class='input-row'>
+              </div>
+              <div class='input-row'>
+              </div>
+              <div class='input-row'>
+              </div>
+              <div class='input-row'>
+              </div>
+            </div>
+          </div>
+
+          <div class='caja-w'>
+            <div class='caja-col'>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/cecilia/fondorevolvente/Seleccion.js
+++ src/components/cecilia/fondorevolvente/Seleccion.js
@@ -3,49 +3,16 @@ import firebase from '../../../Firebase';
 import './Altas.css';
 
 export default class Seleccion extends Component {
-  constructor () {
-    super();
-    this.state = {
-      select: [],
-    };
-  }
-
-  componentWillMount () {
-    firebase.database().ref('presupuesto').on('child_added', snapshot => {
-      this.setState({
-        select: this.state.select.concat(snapshot.val())
-      });
-    });
-  }
-
-  updateSearch(event) {
-    this.setState({search: event.target.value.substr(0,20)});
-  }
 
   render() {
-
-    let filterData = this.state.select.filter(
-      (select) => {
-        return select.ogasto.indexOf(this.state.search) !== -1;
-      }
-    );
-
     return (
       <div class='container-back'>
         <div class='site'>
           <p class='site-s'><b>Caja</b></p>
         </div>
-        <div className="App" style={{height: '100vh'}}>
-          <h1>Citas</h1>
-          <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)}/>
-          {
-            filterData.map(select => (
-              <div className="products-al">
-                <div className="data-table">{select.total}</div>
-              </div>
-            )).reverse()
-          }
-        </div>
+      <div>
+      <select>Aqui van algunas cosas</select>
+      </div>
       </div>
     )
   }
