188b6ec88cc3eed17be81a9ab06cff8226a01f5b
SaulSandovalM <sauldevelop@gmail.com>
2020-05-13 20:57:39 -0500

    para alme

---
+++ src/components/elizabeth/vales/Vales.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Vales.css';
+
+export default class Vales extends Component {
+  render() {
+    return (
+      <div class="container-edit" style={{marginTop: '50px'}}>
+      </div>
+    );
+  }
+}
