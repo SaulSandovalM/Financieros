b06874da153b29047e68a751b2dfc61558b6ad88
Alme95 <masc956@gmail.com>
2020-05-26 17:12:53 -0500

    popup

--- src/components/elizabeth/vales/RowComponent.js
+++ src/components/elizabeth/vales/RowComponent.js
@@ -3,6 +3,8 @@ import './Vales.css';
 import CurrencyFormat from 'react-currency-format';
 import { Link } from 'react-router-dom';
 import Popup from "reactjs-popup";
+import Dropzone from 'react-dropzone';
+
 
 export default class RowComponent extends Component {
   constructor(props){
@@ -38,9 +40,85 @@ export default class RowComponent extends Component {
             .00
           </div>
         </div>
-        <Link to="/Comprobacion" class='table-v-cantidad'>
-          <button>Agregar Comprobación</button>
-        </Link>
+         <div class = 'table-v-cantidad'>
+          <Popup trigger = {<button>Comprobación</button>} modal>
+          <div>
+            <div className='presupuesto-container'>
+              <div className='presupuesto-content'>
+                <div className='presupuesto-card'>
+                  <h1 className='presupuesto-h1'>Comprobaciones</h1>
+                  <p className='presupuesto-p'>Selecciona la carga de evidencias de tus comprobaciones</p>
+                  <button>Mostrar información</button><br/><br/>
+                  <div id="info"></div>
+                  <div>
+                    <p>Facturas:</p>
+                    <Dropzone
+                      style={{
+                        position: 'relative',
+                        width: '100%',
+                        height: '30px',
+                        borderWidth: '2px',
+                        borderColor: 'rgb(102, 102, 102)',
+                        borderStyle: 'solid',
+                        borderRadius: '5px',
+                        maxFiles: 5}}
+                        //accept=".xml" onChange={this.handleOnChange1.bind(this)}
+                        >
+                    </Dropzone>
+                    //<progress className='progress' value={this.state.pdf1} max='100'>
+                      //{this.state.pdf1} %
+                    //</progress>
+                    //<div className="dz-default dz-message" value={this.state.pdf1} max='100'>
+                      //Carga {this.state.pdf1} %</div>
+                  //</div>
+                  <div>
+                    <p>XML:</p>
+                    <Dropzone
+                      style={{
+                        position: 'relative',
+                        width: '100%',
+                        height: '30px',
+                        borderWidth: '2px',
+                        borderColor: 'rgb(102, 102, 102)',
+                        borderStyle: 'solid',
+                        borderRadius: '5px',
+                        maxFiles: 5}}
+                        //accept=".pdf" onChange={this.handleOnChange2.bind(this)}
+                        >
+                    </Dropzone>
+                    //<progress className='progress' value={this.state.pdf2} max='100'>
+                      //{this.state.pdf2} %
+                    //</progress>
+                    //<div className="dz-default dz-message" value={this.state.pdf2} max='100'>
+                      //Carga {this.state.pdf2} %</div>
+                  //</div>
+                  <div>
+                    <p>Recibo Simple:</p>
+                    <Dropzone
+                      style={{
+                        position: 'relative',
+                        width: '100%',
+                        height: '30px',
+                        borderWidth: '2px',
+                        borderColor: 'rgb(102, 102, 102)',
+                        borderStyle: 'solid',
+                        borderRadius: '5px',
+                        maxFiles: 5}}
+                        //accept=".pdf" onChange={this.handleOnChange3.bind(this)}
+                        >
+                    </Dropzone>
+                    //<progress className='progress' value={this.state.pdf3} max='100'>
+                      //{this.state.pdf3} %
+                    //</progress>
+                    //<div className="dz-default dz-message" value={this.state.pdf3} max='100'>
+                      //Carga {this.state.pdf3} %</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </Popup>
+         </div>
         <div class='table-right'>
         </div>
 
