8f009416d57ff12ee3c6cf03c7fdf8d85a69064c
SaulSandovalM <sauldevelop@gmail.com>
2020-07-06 17:02:32 -0500

    menu position

--- src/components/common/nav/Menu.styled.js
+++ src/components/common/nav/Menu.styled.js
@@ -10,7 +10,7 @@ export const StyledMenu = styled.nav`
   width: 13%;
   text-align: left;
   padding: 0rem 2rem 0rem 2rem;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   transition: transform 0.3s ease-in-out;
