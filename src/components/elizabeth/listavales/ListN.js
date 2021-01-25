56f7c07cfa294b5f29704126235cae468bae56f0
SaulSandovalM <sauldevelop@gmail.com>
2021-01-25 16:31:29 -0600

    boton de exportar a pdf en pendientes

--- src/components/elizabeth/listavales/ListN.js
+++ src/components/elizabeth/listavales/ListN.js
@@ -8,6 +8,7 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
+import XLSX from 'xlsx'
 
 export default class ListN extends Component {
   constructor (props) {
@@ -15,6 +16,7 @@ export default class ListN extends Component {
     this.state = {
       vales: []
     }
+    this.handleExcel = this.handleExcel.bind(this)
   }
 
   componentWillMount () {
@@ -25,19 +27,36 @@ export default class ListN extends Component {
     })
   }
 
+  handleExcel () {
+    const vales = [['#V', '#C', 'AUTORIZADO', 'COMPOBADO', 'REEM/REIN', 'CONCEPTO',
+      'OFICIO S', 'AREA', 'TURNO', 'FACTURA', 'RECIBOS', 'S/C', 'FECHA', 'RECIBIO']]
+    this.state.vales.forEach((vale) => {
+      const valeArray = [vale.vale, vale.cheque, vale.cantidad, vale.cantidadc,
+        vale.cantidadr, vale.concepto, vale.oficioS, vale.area,
+        vale.turno, vale.factura, vale.recibos, vale.sc, vale.fecha,
+        vale.personaR]
+      vales.push(valeArray)
+    })
+    const wb = XLSX.utils.book_new()
+    const wsAll = XLSX.utils.aoa_to_sheet(vales)
+    XLSX.utils.book_append_sheet(wb, wsAll, 'Vales')
+    XLSX.writeFile(wb, 'Vales_No_Autorizados.xlsx')
+  }
+
   render () {
     return (
       <div>
+        <div className='but-exc'>
+          <button className='input-sc boton-g' onClick={this.handleExcel}>
+            Exportar a Excel
+          </button>
+        </div>
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell className='table-v-num2'>
-                  <b>Vale</b>
-                </TableCell>
-                <TableCell className='table-v-num2'>
-                  <b>Cheque</b>
-                </TableCell>
+                <TableCell className='table-v-num2'><b>Vale</b></TableCell>
+                <TableCell className='table-v-num2'><b>Cheque</b></TableCell>
                 <TableCell className='table-v-num'><b>Autorizado</b></TableCell>
                 <TableCell className='table-v-num'><b>Comprobado</b></TableCell>
                 <TableCell className='table-v-num'><b>Reintegro</b></TableCell>
