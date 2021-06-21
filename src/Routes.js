e69601b14c4ed654664574d63942e6eb5fc30a52
SaulSandovalM <sauldevelop@gmail.com>
2021-06-21 09:05:19 -0500

    regresando

--- src/Routes.js
+++ src/Routes.js
@@ -1,115 +1,32 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import configureStore from './store/configureStore'
-// Material Design
-import AppBar from '@material-ui/core/AppBar'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Divider from '@material-ui/core/Divider'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-// iconos
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'
-import { makeStyles } from '@material-ui/core/styles'
+import { useOnClickOutside } from './hooks'
+import FocusLock from 'react-focus-lock'
+import Burger from './components/common/nav/Burger'
+import Menu from './components/common/nav/Menu'
 
 const store = configureStore()
-const drawerWidth = 240
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex'
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
-  drawerContainer: {
-    overflow: 'auto'
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    marginTop: '70px'
-  }
-}))
-
-function Routes (props) {
-  const classes = useStyles()
+function Routes () {
+  const [open, setOpen] = useState(false)
+  const node = useRef()
+  const menuId = 'main-menu'
+  useOnClickOutside(node, () => setOpen(false))
 
   return (
-    <div className={classes.root}>
+    <div>
       <Provider store={store}>
         <Router>
-          <CssBaseline />
-          <AppBar position='fixed' className={classes.appBar}>
-            <Toolbar>
-              <Typography variant='h6' noWrap>
-                Dirección de Recursos Finacieros
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Drawer
-            className={classes.drawer}
-            variant='permanent'
-            classes={{
-              paper: classes.drawerPaper
-            }}
-          >
-            <Toolbar />
-            <div className={classes.drawerContainer}>
-              <List>
-                {['Archivos'].map((text, index) => (
-                  <ListItem button key={text}>
-                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : ''}</ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItem>
-                ))}
-              </List>
-              <Divider />
-              <List>
-                {['Presupuesto', 'Fondo Revolvente', 'Archivos', 'Registro', 'Disponible', 'Contrarecibo', 'Carga Contrarecibo'].map((text, index) => (
-                  <ListItem button key={text}>
-                    <ListItemIcon>{index % 10 === 0 ? <MonetizationOnIcon /> : ''}</ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItem>
-                ))}
-              </List>
-              <Divider />
-              <List>
-                {['Presupuesto', 'Fondo Revolvente', 'Archivos', 'Registro', 'Disponible', 'Contrarecibo', 'Carga Contrarecibo'].map((text, index) => (
-                  <ListItem button key={text}>
-                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : ''}</ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItem>
-                ))}
-              </List>
-              <Divider />
-              <List>
-                {['Presupuesto', 'Fondo Revolvente', 'Archivos', 'Registro', 'Disponible', 'Contrarecibo', 'Carga Contrarecibo'].map((text, index) => (
-                  <ListItem button key={text}>
-                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : ''}</ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-          </Drawer>
-          <main className={classes.content}>
-            <App />
-          </main>
+          <div ref={node}>
+            <FocusLock disabled={!open}>
+              <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+              <Menu open={open} setOpen={setOpen} id={menuId} />
+            </FocusLock>
+          </div>
+          <App />
         </Router>
       </Provider>
     </div>
--- src/components/common/home/Common.js
+++ src/components/common/home/Common.js
@@ -1,67 +1,20 @@
 import React, { Component } from 'react'
-// importaciones necesarias
-import CurrencyFormat from 'react-currency-format'
-// Material design
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
-import Divider from '@material-ui/core/Divider'
-// css
 import './Common.css'
 
 export default class Common extends Component {
   render () {
     return (
-      <div className='home-container'>
-        <div className='home-over'>
-          <div className='home-over-font'>VISTA GENERAL</div>
-          <div className='home-usuario-font'>Buenos dias Usuario</div>
-          <div>Esto es lo que está sucediendo con tu presupuesto</div>
-        </div>
-        <div className='home-cards-content'>
-          <div className='home-card'>
-            <div className='home-grafic-content'>
-              <div className='home-grafica'>
-                Grafica
-              </div>
-              <div className='home-text-graf'>
-                <div className='home-money-size'>
-                  <CurrencyFormat
-                    value={299}
-                    displayType='text'
-                    thousandSeparator
-                    prefix=' $ '
-                  /> MXN
-                </div>
-                Afectacion Presupuestal
-              </div>
-            </div>
-            <div>
-              Ver toda la actividad <ArrowForwardIcon />
-            </div>
+      <div className='common'>
+        <div className='common-container'>
+          <div className='common-content'>
+            <p className='common-p'>
+              Bienvenido a Financieros
+            </p>
           </div>
-          <div className='home-card'>
-            <div className='home-grafic-content'>
-              <div className='home-grafica'>
-                Grafica
-              </div>
-              <div className='home-text-graf'>
-                <div className='home-money-size'>
-                  <CurrencyFormat
-                    value={299}
-                    displayType='text'
-                    thousandSeparator
-                    prefix=' $ '
-                  /> MXN
-                </div>
-                Afectacion Presupuestal
-              </div>
-            </div>
-            <Divider />
-            <div style={{ display: 'flex' }}>
-              <div style={{ marginTop: '2px' }}>Ver toda la actividad</div>
-              <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: 'auto' }}>
-                <ArrowForwardIcon />
-              </div>
-            </div>
+          <div className='common-content'>
+            <p className='common-s'>
+              Dirigete al menu para ir a la seccion que desees
+            </p>
           </div>
         </div>
       </div>
